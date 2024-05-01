991a283cc929ff176474d0bc090ccbc21d15401c
Mahmoud Medhat <M7moooud.17@gmail.com>
2024-05-01 20:38:14 +0300

    Added new logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-// import { logo } from "../Utils/Constants";
+import { logo } from "../Utils/Constants";
 import SearchBar from "./SearchBar";
 
 const Navbar = () => {
@@ -12,7 +12,7 @@ const Navbar = () => {
                 sx={{ backgroundColor: "#000", position: "sticky", top: 0, display: "flex", justifyContent: "space-between" }}
             >
                 <Link to={"/"} style={{ display: "flex", alignItems: "center" }}>
-                    <img src="../../public/youtube.png" alt="logo" height={45} />
+                    <img src={logo} alt="logo" height={45} />
                 </Link>
                 <SearchBar />
             </Stack>
